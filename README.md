b03b473658d7e640b7b6acdf5f5a3ae70b6e51be
Daiki Ihara <sasurau4@gmail.com>
2019-12-28 11:45:33 +0900

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+This repository of reproduce Phenyl issue of https://github.com/phenyl/phenyl/issues/298
+
+```
+yarn
+yarn type-check
+```
--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
 import PhenylHttpClient from "@phenyl/http-client";
-import { createEntityClient } from "@phenyl/memory-db";
-import PhenylRestApi from "@phenyl/rest-api";
